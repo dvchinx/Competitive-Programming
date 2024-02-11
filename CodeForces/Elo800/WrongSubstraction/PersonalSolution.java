5feed8f33ae740f9472af5df4bf866e9edb08fb1
David Flórez <71992811+dvchinx@users.noreply.github.com>
2024-02-11 15:13:41 -0500

    Create PersonalSolution.java

---
+++ CodeForces/Elo800/WrongSubstraction/PersonalSolution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class WrongSubstraction {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+
+        scan.close();
+
+        for (int i = 0; i < k; i++) {
+            if(n%10==0){
+                n=n/10;
+            }else{
+                n--;
+            }
+        }
+
+        System.out.println(n);
+    }
+}
