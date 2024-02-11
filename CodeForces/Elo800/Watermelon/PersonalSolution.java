270e71602ed9fd2633db94dfd00721e712708a98
David Flórez <71992811+dvchinx@users.noreply.github.com>
2024-02-11 14:34:09 -0500

    Create PersonalSolution.java

---
+++ CodeForces/Elo800/Watermelon/PersonalSolution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Watermelon {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int w = scan.nextInt();
+        scan.close();
+
+        if(w%2==0 && w>2){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
