4bfbd9eb99535f875028439dce3262339a379241
David Flórez <71992811+dvchinx@users.noreply.github.com>
2024-02-11 14:57:52 -0500

    Create BestSolution.java

---
+++ CodeForces/Elo800/WayTooLongWords/BestSolution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ 
+public class TooLongWords2 {
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		for(int i = 0; i <= n; i++) {
+			String s = in.nextLine();
+			if (s.length() > 10) 
+				System.out.println(s.charAt(0) + "" + (s.length() - 2) + "" + s.charAt(s.length() - 1));
+			else
+				System.out.println(s);
+		}
+		in.close();
+	}
+ 
+}
