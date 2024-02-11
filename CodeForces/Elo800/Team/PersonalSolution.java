a8434ceeea7f3ab6a69133da0ba959a9aa427b36
David Flórez <71992811+dvchinx@users.noreply.github.com>
2024-02-11 15:06:02 -0500

    Create PersonalSolution.java

---
+++ CodeForces/Elo800/Team/PersonalSolution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Team {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int problems = scan.nextInt();
+        int solutions[][] = new int[problems][3];
+
+        int contador = 0;
+
+        for (int i = 0; i < problems; i++) {
+            solutions[i][0] = scan.nextInt();
+            solutions[i][1] = scan.nextInt();
+            solutions[i][2] = scan.nextInt();
+        }
+
+        for (int i = 0; i < problems; i++) {
+            if(solutions[i][0]+solutions[i][1]+solutions[i][2]>=2){
+                contador++;
+            }
+        }
+        scan.close();
+        
+        System.out.println(contador);
+    }
+}
